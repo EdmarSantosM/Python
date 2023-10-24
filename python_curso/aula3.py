8804b228810250c9c0482a1505a755d582dad430
EdmarSantosM <santosem4@hotmail.com>
2023-10-24 19:58:18 -0300

    Update aula3.py

--- python_curso/aula3.py
+++ python_curso/aula3.py
@@ -7,9 +7,24 @@
 """
 
 # aspas simples
+print('Edmar Santos')
 
 # aspas duplas
+print("Edmar Santos")
 
+print()
+
+# Outra forma pode utilizar
+print('"Jose de Alencar"')
+print("'Xavier de Toledo'")
+
+print()
+
+#Esses  dois tipos não são tão usuais 
 # escape
+print("Edmar \"Santos\"")
 
 # r
+print(r"Edmar \"Santos\"") # Exiber todos caracteres - Ver utilização em expressoes regulares
+
+print()
