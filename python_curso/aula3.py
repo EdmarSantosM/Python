c9d0d8393a5697e727e5d186dad4aba75405773c
EdmarSantosM <santosem4@hotmail.com>
2023-10-24 19:45:32 -0300

    Create aula3.py

---
+++ python_curso/aula3.py
@@ -0,0 +1,15 @@
+"""
+    Python --> Linguagem de programação
+    Tipagem => Dinâmica / Forte
+    str --> string --> Conjunto de caracteres (texto)
+    Strings são textos que estão dentro de apas
+
+"""
+
+# aspas simples
+
+# aspas duplas
+
+# escape
+
+# r
