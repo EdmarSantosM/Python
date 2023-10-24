7a64e572aa92f5a0c7715f2f2311ffa465490ec8
EdmarSantosM <santosem4@hotmail.com>
2023-10-24 20:13:55 -0300

    Create aula4.py

---
+++ python_curso/aula4.py
@@ -0,0 +1,21 @@
+# Tipos int e float
+# int --> Numero inteiro ( O tipo int representa qualquer numero positivo ou negativo, inteiro sem sinal e considerado positivo)
+# Exemplo : -2 , -7, 5, 14, 300, 1500 ...
+print()
+print(10)
+print(-11)
+print(0)
+
+print()
+# float --> Numero com ponto flutuante
+# O Tipo float representa qualuqer numero positivo ou negativo com casas decimais
+# Observacao na programação  utiliza-se o ('.') sinal de ponto como separador de casas decimais
+# Exemplo : 1.1 ,  10.50
+print(50.3)
+print(1.9)
+
+print()
+
+# A funcao 'type' mostra o tipo que  Python inferiu ao valor
+# Exemplo :
+
