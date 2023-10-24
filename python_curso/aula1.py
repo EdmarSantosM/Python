e5b65a974be1c5a275952eb1da4e7201b6e2187a
EdmarSantosM <santosem4@hotmail.com>
2023-10-24 19:13:15 -0300

    Create aula1.py

---
+++ python_curso/aula1.py
@@ -0,0 +1,13 @@
+# Permite escrever um comentario no código
+print(123) # Na frente
+
+# Docstring
+"""
+Consigo escrever  
+aqui
+em linhas diferentes
+
+"""
+# outra forma de Docstring
+''' Inserir / escrever suas notas '''
+print(456)
