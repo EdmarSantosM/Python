b9543640bdb0e272ca7c78a40793d812e42e9bce
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 20:54:17 -0300

    Update aula9.py

--- python_curso/aula9.py
+++ python_curso/aula9.py
@@ -1,35 +1,43 @@
 
 adicao = 10 + 10
-print('Adição', adicao)
+print('Adição de 10 + 10 :', adicao)
 
 print()
 
 subtracao = 10 - 5
-print('Subtração', subtracao)
+print('Subtração de 10 - 5 :', subtracao)
 
 print()
 
 multiplicacao = 10 * 10
-print('Multiplicação', multiplicacao)
+print('Multiplicação 10 * 10 :', multiplicacao)
 
 print()
 
-divisao = 10 / 2.2
-print('Divisão', divisao)
+divisao = 10 / 2.2   # float
+print('Divisão de 10 / 2.2 :', divisao)
 
 print()
 
-divisao_inteira = 10 // 2.2
-print('Divisao inteira', divisao_inteira)
+divisao_inteira = 10 // 2.2  # corta as casas decimais
+print('Divisao inteira de 10 // 2.2 :', divisao_inteira)
 
 print()
 
 exponenciacao = 2 ** 10
-print('Exponenciação', exponenciacao)
+print('Exponenciação de 2 ** 10 :', exponenciacao)
 
 print()
 
-modulo = 55 % 2 # resto da divisão
-print('Módulo', modulo)
+modulo = 55 % 2 # Módulo
+print('Resto da divisão de 55 % 2 :', modulo)
+
+print()
+
+valor_par = 10 % 2 == 0
+valor_impar = 10 % 3 == 0
+
+print('Resto da divisao de 10 % 2 == 0 = É par:', valor_par)
+print('Resto da divisao de 10 % 3 == 0 = É ímpar:',valor_impar)
 
 print()
