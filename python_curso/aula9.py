a5b11f8c7c51e26aeb947b607fb038c6b59ca081
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 20:38:16 -0300

    Create aula9.py
    Operadores aritméticos

---
+++ python_curso/aula9.py
@@ -0,0 +1,35 @@
+
+adicao = 10 + 10
+print('Adição', adicao)
+
+print()
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+print()
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+print()
+
+divisao = 10 / 2.2
+print('Divisão', divisao)
+
+print()
+
+divisao_inteira = 10 // 2.2
+print('Divisao inteira', divisao_inteira)
+
+print()
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+print()
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
+
+print()
