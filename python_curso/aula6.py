26af6903bb4c310aaef0dbbb0efe4fa1e22829f5
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 19:10:10 -0300

    Create aula6.py
    Conversão de Tipos de  dados
    
    Nomeclaturas :
    
    Conversão de tipos, coerção, type convertion, typecasting, coercion.

---
+++ python_curso/aula6.py
@@ -0,0 +1,13 @@
+
+# Convertendo tipos de dados.
+
+# Exemplo
+print('1' + 1)
+
+# É apresentado a senguinte mensagem :
+
+#  File "d:\vsCode\curso_python\Python\python_curso\aula6.py", line 5, in <module>
+#     print('1' + 1)
+#           ~~~~^~~
+# TypeError: can only concatenate str (not "int") to str
+
