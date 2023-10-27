f75be0905406a90725c82e5709d76b3af9cf559a
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 14:48:56 -0300

    Update aula5.py

---
+++ python_curso/aula5.py
@@ -0,0 +1,17 @@
+# Tipo de dado bool (boolean)
+
+# Exemplo ao questionar algo em um programa só existe duas respostas possíveis:
+# sim (True) ou não (False).
+
+# Existem vários operadores para "questionar", dentre  eles o  ==, que é um operador lógico 
+# verifica se um valor é igual a outro.
+
+print(10 == 10) # Sim => True (verdadeiro)
+print(10 == 11) # Não => False (falso)
+
+print()
+
+print(type(20 == 20))
+print(type(20 == 30))
+
+print()
