d5da6cbe5b4a0ccb1632784391ca3df2cd0abe95
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 19:46:01 -0300

    Create aula7.py
    Introdução a variáveis em Python.

---
+++ python_curso/aula7.py
@@ -0,0 +1,17 @@
+# Variaveis são usadas para  salvar algo na memória do computador
+
+# PEP8 : Guia de estilo do Código Python
+# Exemplo na declaração de variáveis, inicie com letras minúsculas, pode-se usar números e underline '_'
+
+#  sinal de = é um operador de atribuição
+# snakecase
+# Exemplo nome_variavel = expressao
+
+# declarando uma variável
+
+# (variable) soma_de_dois_mais_dois: Literal[4]
+soma_de_dois_mais_dois = 2 + 2
+print(soma_de_dois_mais_dois)
+
+nome_completo = 'Edmar Santos'
+print(nome_completo) #Edmar Santos
