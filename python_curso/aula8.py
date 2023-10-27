89841a944e67f86977310bdcdfe7f22a9fbbe294
EdmarSantosM <santosem4@hotmail.com>
2023-10-27 20:18:56 -0300

    Create aula8.py
    Exercícios com variáveis  e tipos de dados.

---
+++ python_curso/aula8.py
@@ -0,0 +1,14 @@
+
+nome = 'Edmar'
+sobrenome = 'Marcelino'
+idade = 41
+ano_nascimento = '24/04/1982'
+maior_de_idade = True
+altura_metros = 1.79
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade )
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_metros)
