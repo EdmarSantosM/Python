7c163fd32fea536fb952719b5a246c059de8e541
EdmarSantosM <santosem4@hotmail.com>
2023-10-30 12:35:41 -0300

    Create aula11.py
    Precedência dos operadores aritméticos

---
+++ python_curso/aula11.py
@@ -0,0 +1,17 @@
+
+# 1. (n + n)
+# 2. **
+# 3. * , / , // , %
+# 4. + , 1
+
+#  1 ** 5
+#  1 + 1 + 5
+conta_1 = 1 + 1 ** 5 + 5  
+print(conta_1)
+
+print()
+
+conta_com_precedencia = (1 + 1) ** (5 + 5) # resultado 1024
+print(conta_com_precedencia)
+
+print()
