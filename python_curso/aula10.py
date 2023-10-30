bcff095706f33bd19ac9f95f9ba973b5dacaf7ab
EdmarSantosM <santosem4@hotmail.com>
2023-10-30 12:21:45 -0300

    Create aula10.py
    Concatenação (+) e Repetição ( * ) com operadores aritméticos

---
+++ python_curso/aula10.py
@@ -0,0 +1,18 @@
+
+# Concatenação
+concatenacao = 'Edmar' + ' ' + 'Santos'
+print(concatenacao)
+
+concatenacao_dois = 'Edmar' + (str(1)) + 'Santos'
+print(concatenacao_dois)
+
+print()
+
+#repetição
+cinco_vezes = 'A' * 5
+print(cinco_vezes)
+
+print()
+
+dez_vezes_hifen = '-' * 10
+print(dez_vezes_hifen)
