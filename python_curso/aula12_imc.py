def53d289f318e945ccd5d3e6ab96cc7e6ea8dbd
EdmarSantosM <santosem4@hotmail.com>
2023-10-30 13:00:24 -0300

    Create aula12_imc.py
    Exercício de programação
    
    Calcular  o IMC de uma  pessoa.

---
+++ python_curso/aula12_imc.py
@@ -0,0 +1,25 @@
+
+# declararção variaveis
+
+nome = 'Edmar'
+peso = 70.0
+altura = 1.79
+
+imc = ... # Ellipsis
+print('... ', '# Ellipsis')
+
+print()
+
+# calcular o IMC
+imc = peso / (altura * altura)
+
+hifen = '-' * 60
+
+print(hifen)
+
+print('Meu nome é ' + nome + ' tenho ' + str(altura)+ ' de altura e peso ' + str(peso) )
+print('Meu IMC é de ' + str(imc))
+
+print(hifen)
+
+print()
