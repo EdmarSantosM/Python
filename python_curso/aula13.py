11ca2f2f3b0a98b112d379685230218206ed4679
EdmarSantosM <santosem4@hotmail.com>
2023-10-30 13:18:32 -0300

    Create aula13.py
    Introdução a f-string (formatações)

---
+++ python_curso/aula13.py
@@ -0,0 +1,19 @@
+
+#variaveis
+nome = 'Edmar'
+sobrenome = 'Santos'
+idade = 41
+peso = 70.0
+altura = 1.79
+
+imc = peso / altura * altura
+
+asteristico = '*' * 80
+
+print(asteristico)
+
+print(f'{nome} {sobrenome} tenho {idade} anos meu peso altual é {peso}kg e minha  altura {altura:.2f}')
+print(f'Meu imc é {imc:.2f}')
+
+print(asteristico)
+print()
