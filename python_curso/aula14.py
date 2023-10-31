177a07fb7a2065937d88882ad6a0a2ce70fb0444
EdmarSantosM <santosem4@hotmail.com>
2023-10-31 16:14:49 -0300

    Update aula14.py
    # ver sobre  argumentos
    
    # ver sobre parametros nomeados

--- python_curso/aula14.py
+++ python_curso/aula14.py
@@ -1,9 +1,23 @@
 # Método format
 
-a = 'A'
-b = 'B'
+# variáveis
+a = 'AaaA'
+b = 'BbbB'
 c = 1.1
 
-formato = ''.format(a, b ,c) 
+string = 'a={} b={} c={:.2f}'
+
+formato = string.format(a , b , c)  
+# ver sobre  argumentos
+# Exemplo ( x, p, t, o)
+
+# ver sobre parametros nomeados 
+# Exemplo : (nome1=a, nome2=b, nome3=c)
+
+string = 'b={nome1} a={nome2} c={nome3:.2f}'
+formato2 = string.format(nome1=a, nome2=b, nome3=c)
+
 
 print(formato)
+print()
+print(formato2)
