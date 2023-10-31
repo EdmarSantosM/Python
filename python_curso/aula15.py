8922082d6bf6fe080cc3deedf0064ff56d18dd81
EdmarSantosM <santosem4@hotmail.com>
2023-10-31 16:50:56 -0300

    Convertendo o  tipos
     É possível já  efetuar  a conversão, porém não recomendada pois pode quebrar o programa

--- python_curso/aula15.py
+++ python_curso/aula15.py
@@ -10,9 +10,26 @@ print(f'O seu nome é {nome}')
 
 print()
 
-nome2 = input('Digite seu nome:')
+nome2 = input('Digite seu nome: ')
 
 print()
 
 print(f'Olá!{nome2=}')
+
+print()
+
+# numero_1 = input('Digite um número: ')
+# numero_2 = input('Digite outro número: ')
+
+# print(f'A soma dos numeros {numero_1 + numero_2}') # Neste  caso sera  concatenado os valores
+
+# Pode fazer, porém não é boa prática
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número'))
+
+print()
+
+print(f'A soma de {numero_1 + numero_2}')
+
+
 
