cd592607f4f03f84036e0316ff13d352d2128dd3
EdmarSantosM <santosem4@hotmail.com>
2023-10-31 16:34:50 -0300

    Função input
    Coletando dados do usuário

---
+++ python_curso/aula15.py
@@ -0,0 +1,19 @@
+# Exemplo
+
+nome = input('Qual seu nome? ')
+print()
+
+print(nome)
+
+print()
+print(f'O seu nome é {nome}')
+
+print()
+
+nome2 = input('Digite seu nome :')
+
+print()
+
+print(f'Olá !{nome2=}')
+
+print(nome2)
