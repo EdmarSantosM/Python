e848977ee5922e5975b441a701cba704629a0f72
EdmarSantosM <santosem4@hotmail.com>
2023-11-07 18:18:00 -0300

    Operadores Relacionais

---
+++ python_curso/aula19.py
@@ -0,0 +1,50 @@
+"""
+Operadores Relacionais no Python
+
+>  maior   
+>= maior igual
+<  menor
+<= menos igual
+== igual
+!= diferente
+
+"""
+
+# Exemplos:
+
+maior = 2 > 1
+
+maior_ou_igual = 2 >= 2
+
+menor = 1 < 2
+
+menor_ou_igual = 2 <= 2
+
+igual = 'a' == 'a'
+
+diferente = 'a' != 'b'
+
+print()
+print(maior)
+print()
+
+
+print()
+print(maior_ou_igual)
+print()
+
+print()
+print(menor)
+print()
+
+print()
+print(menor_ou_igual)
+print()
+
+print()
+print(igual)
+print()
+
+print()
+print(diferente)
+print()
