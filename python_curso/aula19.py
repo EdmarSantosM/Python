dc3c18c5d38c18a6485b20227161ebc728759fe8
EdmarSantosM <santosem4@hotmail.com>
2023-11-07 18:40:13 -0300

    Update aula19.py

--- python_curso/aula19.py
+++ python_curso/aula19.py
@@ -25,26 +25,26 @@ igual = 'a' == 'a'
 diferente = 'a' != 'b'
 
 print()
-print(maior)
+print('maior = 2 > 1'  , maior)
 print()
 
 
 print()
-print(maior_ou_igual)
+print('maior_ou_igual = 2 >= 2 ' , maior_ou_igual)
 print()
 
 print()
-print(menor)
+print('menor = 1 < 2 ' , menor)
 print()
 
 print()
-print(menor_ou_igual)
+print('menor_ou_igual = 2 <= 2' , menor_ou_igual)
 print()
 
 print()
-print(igual)
+print( "'a' == 'a' " , igual)
 print()
 
 print()
-print(diferente)
+print( "diferente = 'a' != 'b'", diferente)
 print()
