f98ed616fd356c4e68baed687fa3e6f49e034eb6
EdmarSantosM <santosem4@hotmail.com>
2023-11-09 17:45:56 -0300

    Update aula25_interpolacao.py

--- python_curso/aula25_interpolacao.py
+++ python_curso/aula25_interpolacao.py
@@ -8,3 +8,14 @@ print(20 * '-')
 
 print(variavel)
 print()
+
+print(40 * '-')
+print()
+print(' O Hexadecimal de %d é %X05' % (1750,1750))
+
+print()
+
+print(40 * '-')
+print()
+print(' O Hexadecimal de %f é %x04' % (654283,654283))
+print()
