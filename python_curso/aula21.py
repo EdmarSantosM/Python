e08c47da61b2262ee833ca1250cdfb35b93f32ea
EdmarSantosM <santosem4@hotmail.com>
2023-11-09 16:55:12 -0300

    Update aula21.py

--- python_curso/aula21.py
+++ python_curso/aula21.py
@@ -1,7 +1,7 @@
 
 # and - Todas as condicoes precisam ser verdadeiras
 
-# Se qualquer valor for considerado falso a expressao inteira será avaliadanaquele valor como falso
+# Se qualquer valor for considerado falso a expressao inteira será avaliada naquele valor 
 
 # Sao consideradas "falsy" (0 , 0.0, '', False)
 
@@ -12,10 +12,31 @@ senha_cadastrada = '123456'
 entrada = input('[E]ntrar [S]air ')
 senha_digitada = input('Senha :')
 
-if(entrada == 'E' and senha_digitada == senha_cadastrada):
+# if(entrada == 'E' and senha_digitada == senha_cadastrada):
+#     print()
+#     print('Entrar no console')
+#     print()
+# else:
+#     print()
+#     print('Sair do console')
+#     print()
+
+
+# # Avaliacao curto circuito
+# hifen = ' - ' * 10
+# print() 
+# print(hifen)
+# print( '\t',True and False and True)
+# print('\t',True and 0 and True)
+# print(hifen)   
+# print()
+
+# Utilizando o operador 'or'
+if( entrada == 'E' or entrada == 'e') and (senha_digitada == senha_cadastrada):
     print()
     print('Entrar no console')
     print()
+
 else:
     print()
     print('Sair do console')
@@ -23,10 +44,14 @@ else:
 
 
 # Avaliacao curto circuito
+
 hifen = ' - ' * 10
 print() 
 print(hifen)
-print( '\t',True and False and True)
-print('\t',True and 0 and True)
+# print( '\t',True and True and True)
+# print('\t',True and 0 and True)
+print('\t',0 or False or 0 or 'abc') # abc foi  considerada como true (verdadeiro).
 print(hifen)   
 print()
+
+
