c6e8ad511b4efa2551054fc7a20836e91880b974
EdmarSantosM <santosem4@hotmail.com>
2023-11-09 16:20:00 -0300

    Operadores  lógicos
    Uso  do and

---
+++ python_curso/aula21.py
@@ -0,0 +1,19 @@
+
+# and - Todas as condicoes precisam ser verdadeiras
+
+# Se qualquer valor for considerado falso a expressao inteira será avaliadanaquele valor como falso
+
+# Sao consideradas "falsy" (0 , 0.0, '', False)
+
+# O tipo None que e usado para representar um 'nao valor'
+
+entrada = input('[E]ntrar [S]air ')
+
+if(entrada == 'E'):
+    print()
+    print('Entrar no console')
+    print()
+else:
+    print()
+    print('Sair do console')
+    print()
