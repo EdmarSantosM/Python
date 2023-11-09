517f517b1d7a10702015eefb7b25767dc974c7d4
EdmarSantosM <santosem4@hotmail.com>
2023-11-09 17:26:57 -0300

    Update aula24_in_not_in.py

--- python_curso/aula24_in_not_in.py
+++ python_curso/aula24_in_not_in.py
@@ -6,6 +6,20 @@
 
 # e d m a r
 
-nome = 'Edmar'
+# nome = 'Edmar'
 # print(nome[2])
-print('m' in nome)
+# print('m' in nome)
+# print('z' in nome)
+# print(15 * '-')
+# print('ma' not in nome)
+# print('z' not in nome)
+
+# Exemplo
+nome_completo = input(' Digite seu nome completo : ')
+encontrar =  input(' Digite a letra que deseja encontrar : ')
+
+if encontrar in nome_completo:
+    print(f'{encontrar} esta em {nome_completo}')
+else:
+    print(f'{encontrar} não está em {nome_completo}')
+
