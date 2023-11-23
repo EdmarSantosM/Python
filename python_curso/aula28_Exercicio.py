1db467f10fb1d42023a3c3cb7668caafcd3b01b0
EdmarSantosM <santosem4@hotmail.com>
2023-11-23 07:51:19 -0300

    Aula 28 - Exercicio de fixação.

---
+++ python_curso/aula28_Exercicio.py
@@ -0,0 +1,37 @@
+"""
+Exercicio :
+
+    Peça para o usuário digitar seu nome
+    Peça para o usuário digitar sua idade
+
+    Se o nome e idade forem digitados:
+        Exiba :
+            Seu nome é {nome}
+            Seu nome invertido é {nome invertido}
+            Seu nome contém (ou não) espaços
+            Seu nome tem {n} letras
+
+            A primeira letra do seu nome é {letra}
+            A última letra do seu nome é {letra}
+
+    Se nada for digitado em nome ou idade : Exiba " Desculpe, você deixou campos vazios."        
+
+"""
+
+# Variaveis
+
+hifen = ' - ' * 30
+
+print(hifen)
+print()
+nome = input('Digite seu nome : ')
+idade = int(input('Digite sua idade : '))
+print()
+print(hifen)
+
+if( nome != "" and idade != ""):
+    print()
+    print(f'Nome invertido :', nome[::-1].upper())
+    print()
+    print(f'Seu nome tem :', {len(nome)} , 'letras')
+
