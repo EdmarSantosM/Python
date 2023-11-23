3294bc582561389d6ff4a638f13d237414323388
EdmarSantosM <santosem4@hotmail.com>
2023-11-23 10:32:30 -0300

    Update aula28_Exercicio.py

--- python_curso/aula28_Exercicio.py
+++ python_curso/aula28_Exercicio.py
@@ -31,15 +31,56 @@ print(hifen)
 
 if( nome != "") and (idade != ""):
     print()
+    print(f'Seu nome é {nome}')
+    print()
     print(f'Nome invertido :', nome[::-1].upper())
     print()
+
+    # validacao de  espaços
+    if ' ' in {nome}:
+        print(f'Seu nome contém espaços')
+    else:
+        print(f'Seu nome NÂO contem espaços')
+    print()
+
     print(f'Seu nome tem :', {len(nome)} , 'letras')
     print()
     print(f'A primeira letra do seu nome :',(nome[:1]))
     print()
     print(f'A última letra do seu nome :' ,(nome[4:5]))
     print()
-elif ( nome == "") or (idade != 41):
+elif ( nome == "") or (idade != ""):
     print()
-    print(f'Desculpe você deixou campo {(nome)} ou  o campo {(idade)} vazios')
+    print(f'Desculpe você deixou ou o campo "nome" ou "idade" vazios')
     print()
+
+print()
+print(hifen)
+print()
+
+# OUTRA SOLUCAO SUGERIDA
+
+nome_2 = input('Digite o seu nome: ')
+idade_2 = input('Digite sua idade: ')
+
+print(hifen)
+print()
+
+if nome_2 and idade_2:
+   print(f'Seu nome é {nome_2}')
+   print(f'Seu nome invertido é  {nome_2[::-1]}')
+
+   if ' ' in nome_2:
+       print(f'Seu nome têm espaços')
+   else:
+       print(f'Seu nome não têm espaços')
+
+   print(f'Seu nome tem {len(nome_2)} letras')
+
+   print(f'A primeira letra do seu nome {nome_2[0]}')
+
+   print(f'A última letra do seu {nome_2[-1]}')
+
+else:
+    print("Desculpe, você deixou campos vazios")
+
